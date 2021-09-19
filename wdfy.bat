0c1acaab1ccaa155ebbedca427ec03350b896a8b
Loserwdfy <91027474+Loserwdfy@users.noreply.github.com>
2021-09-19 13:03:12 -0500

    Add files via upload
    Heres a simple rainbow pinger, that you can download on school computer or any pc you want as long as github is not blocked, enjoy!

---
+++ wdfy.bat
@@ -0,0 +1,75 @@
+title IP Rainbow Pinger - Made by wdfy on tiktok
+echo off & cls
+color c
+con lines=20 cols=70
+echo               ...                                
+echo              ;::::;                              
+echo            ;::::; :;                              
+echo          ;:::::'   :;                             
+echo         ;:::::;     ;.                           
+echo        ,:::::'       ;           OOO\           
+echo        ::::::;       ;          OOOOO\            
+echo        ;:::::;       ;         OOOOOOOO           
+echo       ,;::::::;     ;'         / OOOOOOO          
+echo     ;:::::::::`. ,,,;.        /  / OOOOOOO        
+echo   .';:::::::::::::::::;,     /  /     OOOOO     
+echo  ,::::::;::::::;;;;::::;,   /  /        OOOO     
+echo ;::::::'::::::;;;::::: ,#/  /          OOOO   
+echo ::::::::;::::::;;::: ;::#  /            OOOO   
+echo ::::::::;:::::::: ;::::# /              OOO   
+echo `:`:::::::`;:::::: ;::::::#/               OOO
+echo  :::`:::::::;; ;:::::::::##                OO
+echo  ::::`:::::::;::::::::;:::#                OO
+echo  `:::::`::::::::::::;':;::#                O
+echo   `:::::`::::::::;' /  / :#
+echo    ::::::`:::::;'  /  /   #                                                                                                                                 
+set /p IP=Enter the script kids IP :
+echo.
+:rainbow
+PING -n 1 %IP% | FIND "TTL="
+IF ERRORLEVEL 1 (SET in=c & echo %IP%  Goofy ass thought he was funny )
+ping -t 2 0 10 127.0.0.1 >nul
+color 01
+PING -n 1 %IP% | FIND "TTL="
+IF ERRORLEVEL 1 (SET in=c & echo %IP%  Goofy ass thought he was funny  )
+ping -t 2 0 10 127.0.0.1 >nul
+color 02
+PING -n 1 %IP% | FIND "TTL="
+IF ERRORLEVEL 1 (SET in=c & echo %IP%  Goofy ass thought he was funny )
+color 3PING -n 1 %IP% | FIND "TTL="
+IF ERRORLEVEL 1 (SET in=c & echo %IP%  Goofy ass thought he was funny )
+color 4
+PING -n 1 %IP% | FIND "TTL="
+IF ERRORLEVEL 1 (SET in=c & echo %IP%  Goofy ass thought he was funny )
+color 5
+PING -n 1 %IP% | FIND "TTL="
+IF ERRORLEVEL 1 (SET in=c & echo %IP%  Goofy ass thought he was funny )
+color 6
+PING -n 1 %IP% | FIND "TTL="
+IF ERRORLEVEL 1 (SET in=c & echo %IP%  Goofy ass thought he was funny )
+color 7
+PING -n 1 %IP% | FIND "TTL="
+IF ERRORLEVEL 1 (SET in=c & echo %IP%  Goofy ass thought he was funny )
+color 8
+PING -n 1 %IP% | FIND "TTL="
+IF ERRORLEVEL 1 (SET in=c & echo %IP%  Goofy ass thought he was funny )
+color 9
+PING -n 1 %IP% | FIND "TTL="
+IF ERRORLEVEL 1 (SET in=c & echo %IP%  Goofy ass thought he was funny )
+color 0b
+PING -n 1 %IP% | FIND "TTL="
+IF ERRORLEVEL 1 (SET in=c & echo %IP%  Goofy ass thought he was funny )
+color 0c
+PING -n 1 %IP% | FIND "TTL="
+IF ERRORLEVEL 1 (SET in=c & echo %IP%  Goofy ass thought he was funny )
+color 0d
+PING -n 1 %IP% | FIND "TTL="
+IF ERRORLEVEL 1 (SET in=c & echo %IP%  Goofy ass thought he was funny )
+color 0e
+PING -n 1 %IP% | FIND "TTL="
+IF ERRORLEVEL 1 (SET in=c & echo %IP%  Goofy ass thought he was funny )
+color 0f
+PING -n 1 %IP% | FIND "TTL="
+IF ERRORLEVEL 1 (SET in=c & echo %IP%  Goofy ass thought he was funny )
+ping -t 2 0 10 127.0.0.1 >nul
+goto rainbow
